6cc8be793d9965fddb640fdb350d373339d15ac4
nvak1999 <76765929+nvak1999@users.noreply.github.com>
2021-07-11 09:07:40 +0700

    asda

--- myExpressApp/routes/index.js
+++ myExpressApp/routes/index.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var router = express.Router();
 const userApi = require("./user.api");
+const productApi = require("./product.api");
 
 router.use("/user", userApi);
+router.use("/product", productApi);
 
 module.exports = router;
